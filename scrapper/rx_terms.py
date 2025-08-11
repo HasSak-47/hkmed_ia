f839885061e5472cbf8874f3552c13427b327210
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-08-10 20:30:39 -0600

    all stubs

--- scrapper/rx_terms.py
+++ scrapper/rx_terms.py
@@ -1,18 +1,36 @@
-def get_all_concepts(*_):
+# see https://lhncbc.nlm.nih.gov/RxNav/APIs/RxTermsAPIs.html
+from typing import List, Optional
+from requests import request
+from sys import argv
+
+from .utils import DOMAIN
+
+def __rx_terms(path: str, **kwargs):
+    """Internal helper function to make API requests"""
+    url = f'{DOMAIN}/REST/RxTerms/{path}.json'
+    r = request('GET', url, params=kwargs if kwargs else None)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def get_all_concepts():
     """
     not implemented
     """
     raise RuntimeError('not implemented')
+
 def get_all_rx_term_info(*_):
     """
     not implemented
     """
     raise RuntimeError('not implemented')
+
 def get_rx_term_display_name(*_):
     """
     not implemented
     """
     raise RuntimeError('not implemented')
+
 def get_rx_terms_version(*_):
     """
     not implemented
--- scrapper/utils.py
+++ scrapper/utils.py
@@ -3,9 +3,3 @@ from requests import request
 
 DOMAIN : Final = "https://rxnav.nlm.nih.gov"
 
-def rx_terms(path: str, **kwargs):
-    url = f'{DOMAIN}/REST/RxTerms/{path}.json'
-    r = request('GET', url, params=kwargs if kwargs else None)
-    if r.status_code != 200:
-        return None
-    return r.json()
